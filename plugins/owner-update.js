7e0d7d8173a73b034cfc73053c1cd2b42db00048
Edwin <edwin313560@gmail.com>
2025-08-08 11:44:03 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,6 +1,6 @@
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
-await m.react('🕓')
+await m.react('⌛')
 if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 await conn.reply(m.chat, stdout.toString(), m, rcanal)
