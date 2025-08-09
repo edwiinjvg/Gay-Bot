750b74edba9bc2584964910ad270b1a948e78e98
Edwin <edwin313560@gmail.com>
2025-08-08 21:18:00 -0500

    Actualizar fun-messi.js

--- plugins/fun-messi.js
+++ plugins/fun-messi.js
@@ -4,21 +4,11 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     let res = (await axios.get(`https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/Messi.json`)).data;
     let url = res[Math.floor(Math.random() * res.length)];
 
-    const buttons = [
-        {
-            buttonId: `.messi`,
-            buttonText: { displayText: "⚜️ Otro" },
-            type: 1
-        }
-    ];
-
     await conn.sendMessage(
         m.chat,
         {
             image: { url },
-            caption: "Aquí tienes.",
-            buttons: buttons,
-            viewOnce: true
+            caption: "🐐",
         },
         { quoted: m }
     );
