8c804928fb6b15c55a51f5faa41f600c5892ea28
Edwin <edwin313560@gmail.com>
2025-08-11 16:22:23 -0500

    Actualizar game-actions+18.js

--- plugins/game-actions+18.js
+++ plugins/game-actions+18.js
@@ -131,7 +131,7 @@ const videoUrls = {
     ],
     cum: [
         'https://telegra.ph/file/9243544e7ab350ce747d7.mp4', 'https://telegra.ph/file/fadc180ae9c212e2bd3e1.mp4', 
-        'https://telegra.ph/file/79a5a0042dd8c44754942.mp4', 'https://telegra.ph/file/035e84b8767a9f1ac070b.mp4', 
+        'https://telegra.ph/file/79a5a0042dd8c44754942.mp4',
         'https://telegra.ph/file/0103144b636efcbdc069b.mp4', 'https://telegra.ph/file/4d97457142dff96a3f382.mp4', 
         'https://telegra.ph/file/b1b4c9f48eaae4a79ae0e.mp4', 'https://telegra.ph/file/5094ac53709aa11683a54.mp4', 
         'https://telegra.ph/file/5094ac53709aa11683a54.mp4', 'https://telegra.ph/file/dc279553e1ccfec6783f3.mp4', 
