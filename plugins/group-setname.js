c8f578ad2d7111a0bd5dd73630603c9609d8fa6b
Edwin <edwin313560@gmail.com>
2025-08-12 09:55:01 -0500

    Crear group-setname.js

---
+++ plugins/group-setname.js
@@ -0,0 +1,22 @@
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) {
+        return conn.reply(m.chat, `_Escribe un nuevo nombre para el grupo._`, m);
+    }
+    
+    try {
+        await conn.groupUpdateSubject(m.chat, text);
+        await conn.reply(m.chat, `_¡El nombre del grupo ha sido cambiado a "${text}" exitosamente!_`, m);
+    } catch (e) {
+        console.error(e);
+        await conn.reply(m.chat, '_Ocurrió un error al intentar cambiar el nombre._', m);
+    }
+};
+
+handler.help = ['setsubject <nuevo nombre>'];
+handler.tags = ['group'];
+handler.command = ['setsubject', 'setname'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
