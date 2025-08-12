05d79369e74bfca466d4c58875465b36a467b4f7
Edwin <edwin313560@gmail.com>
2025-08-12 10:52:22 -0500

    Crear group-link.js

---
+++ plugins/group-link.js
@@ -0,0 +1,19 @@
+var handler = async (m, { conn, usedPrefix, command }) => {
+    try {
+        // Obtener el enlace de invitación del grupo
+        const groupLink = await conn.groupInviteCode(m.chat);
+        
+        await conn.reply(m.chat, `- _¡Aquí está el enlace de invitación del grupo!_\n- _*https://chat.whatsapp.com/${groupLink}*_`, m);
+    } catch (e) {
+        console.error(e);
+        await conn.reply(m.chat, '_Ocurrió un error al obtener el enlace._', m);
+    }
+};
+
+handler.help = ['link', 'enlace'];
+handler.tags = ['group'];
+handler.command = ['link', 'enlace'];
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
